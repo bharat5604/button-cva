a9634a5b029c8e22e093e7c22edff8365a9d3bde
bharat <bhushansingh696@gmail.com>
2023-06-05 00:51:56 +0530

    adding auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,9 @@
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [],
+  session: { strategy: "jwt" },
+  pages: {
+    signIn: "/login",
+  },
+};
